a8734289465d90da0adf5477dfcf706194a2eeb2
Sergei Anishchenko <2848526+TheSirop@users.noreply.github.com>
2022-11-18 00:52:50 +0300

    Update wallpaper-install.sh

--- wallpaper/wallpaper-install.sh
+++ wallpaper/wallpaper-install.sh
@@ -3,7 +3,9 @@
 echo "Copying wallpaper files..."
 mkdir -p $HOME/.wallpaper
 cp *.jpg $HOME/.wallpaper
+sleep 2
 cp wallpaper-run.sh $HOME/.wallpaper
 chmod 755 $HOME/.wallpaper/wallpaper-run.sh
+bash $HOME/.wallpaper/wallpaper-run.sh
+sleep 2
 echo "All done!"
-
