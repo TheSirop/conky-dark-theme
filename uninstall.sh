deff84f35b7a55dbbc726c0a2be2b0aed5855d40
Sergei Anishchenko <2848526+TheSirop@users.noreply.github.com>
2022-11-17 12:15:44 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,5 +7,8 @@ echo ".wallpaper folder delete"
 rm -rf -p $HOME/.conky/
 sleep 2
 echo ".conky folder delete"
+rm -r -p $HOME/.conkyrc
+sleep 2
+echo ".conky config delete"
 sleep 2
 echo "All done!"
