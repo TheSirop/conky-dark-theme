ed6b659beb3dfe1cc4b6fa235e40089cea7babc8
Sergei Anishchenko <mail.anishchenko@gmail.com>
2022-11-18 09:07:43 +0300

    Modify setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-echo "Copying Conky config..."
+echo "Install..."
+
+sleep 2
 cp .conkyrc $HOME/
+echo ".conkyrc done!"
+
+sleep 2
+bash fonts/install.sh
+echo "fonts done!"
+
+sleep 2
+bash spotify/install.sh
+echo "spotify done!"
+
+sleep 2
+bash wallpaper/install.sh
+echo "wallpaper done!"
+
+sleep 2
 echo "All done!"
